fa0a19740ce9089b8b27f13276a9c541d2d30945
Luigi Cavalieri <58509573+LuigiCavalieri@users.noreply.github.com>
2024-09-23 16:42:06 +0200

    Added "quote saving" state.

--- fe-react/src/components/QuoteForm/QuoteForm.tsx
+++ fe-react/src/components/QuoteForm/QuoteForm.tsx
@@ -1,4 +1,4 @@
-import { ChangeEvent, useState, FormEvent, useCallback } from "react";
+import { useState, FormEvent, useRef } from "react";
 import appConfig from "../../config/appConfig";
 import { useSaveQuote } from "../../hooks/quotes";
 import SubmitButton from "../SubmitButton/SubmitButton";
@@ -9,48 +9,33 @@ import { QuoteWithoutServerGenFields } from "../../types/quotes";
 import { useTimer } from "../../hooks/timer";
 
 export default function QuoteForm() {
-	const { setTimer, clearTimer } = useTimer();
+	const { setTimer } = useTimer();
+	const formRef = useRef<HTMLFormElement | null>(null);
 	const [submitEnabled, setSubmitEnabled] = useState(false);
-	const [showOptimisticSaved, setShowOptimisticSaved] = useState(false);
-	const [formValues, setFormValues] = useState(initFormValues);
+	const [showSaved, setShowSaved] = useState(false);
 
-	const { isError, error, mutate } = useSaveQuote({
-		onError: (_error, variables) => {
-			clearTimer();
-			setShowOptimisticSaved(false);
-			setFormValues(variables);
-			setSubmitEnabled(true);
+	const { isError, isLoading, error, mutate } = useSaveQuote({
+		onSuccess: () => {
+			formRef.current?.reset();
+			setSubmitEnabled(false);
+			setShowSaved(true);
+			setTimer(() => {
+				setShowSaved(false);
+			}, appConfig.feedbackTimeout);
 		},
 	});
 
 	const handleOnSubmit = (event: FormEvent<HTMLFormElement>) => {
 		event.preventDefault();
 
-		setFormValues(initFormValues);
-		setSubmitEnabled(false);
-		setShowOptimisticSaved(true);
-		mutate(formValues);
+		const data = new FormData(event.target as HTMLFormElement);
 
-		setTimer(() => {
-			setShowOptimisticSaved(false);
-		}, appConfig.feedbackTimeout);
+		mutate(Object.fromEntries(data.entries()) as QuoteWithoutServerGenFields);
 	};
 
-	const handleOnChange = useCallback(
-		(event: ChangeEvent<HTMLTextAreaElement | HTMLInputElement>) => {
-			const value = event.target.value;
-
-			setFormValues({
-				...formValues,
-				[event.target.name]: value,
-			});
-
-			if (event.target.tagName === "TEXTAREA") {
-				setSubmitEnabled(Boolean(value.trim()));
-			}
-		},
-		[formValues]
-	);
+	const handleOnChange = (value: string) => {
+		setSubmitEnabled(Boolean(value.trim()));
+	};
 
 	return (
 		<Card>
@@ -59,34 +44,34 @@ export default function QuoteForm() {
 					{error.message}
 				</ErrorMessage>
 			)}
-			<form data-testid="quote-form" className="grid gap-5 py-2" onSubmit={handleOnSubmit}>
+			<form
+				data-testid="quote-form"
+				className="grid gap-5 py-2"
+				ref={formRef}
+				onSubmit={handleOnSubmit}
+			>
 				<textarea
 					required
 					className="leading-6 outline-none p-2 border rounded-sm border-slate-300 h-40 resize-none disabled:text-slate-400 focus:border-sky-500"
 					name="content"
 					placeholder="Type in the quote you want to save"
-					value={formValues.content}
-					onChange={handleOnChange}
+					disabled={isLoading}
+					onChange={event => handleOnChange(event.target.value)}
 				/>
 				<TextField
 					type="name"
 					name="author"
 					placeholder="by 'anonymous'"
+					disabled={isLoading}
 					maxLength={appConfig.authorNameMaxLength}
-					value={formValues.author!}
-					onChange={handleOnChange}
 				/>
 				<div className="flex items-center justify-end gap-2">
-					{showOptimisticSaved && (
-						<span className="font-medium text-green-700 text-sm">Saved!</span>
-					)}
-					<SubmitButton disabled={!submitEnabled} className="w-1/4">
-						Save
+					{showSaved && <span className="font-medium text-green-700 text-sm">Saved!</span>}
+					<SubmitButton disabled={!submitEnabled || isLoading} className="w-1/4">
+						{isLoading ? "Saving..." : "Save"}
 					</SubmitButton>
 				</div>
 			</form>
 		</Card>
 	);
 }
-
-const initFormValues: QuoteWithoutServerGenFields = { content: "", author: "" };
