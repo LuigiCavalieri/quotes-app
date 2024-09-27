aa97686deb4f3c46fcf3416376788d9951a2ad9e
Luigi Cavalieri <58509573+LuigiCavalieri@users.noreply.github.com>
2024-09-27 09:38:26 +0200

    Added author name fallback value.

--- fe-angular/src/app/components/quotes-list-item/quotes-list-item.component.ts
+++ fe-angular/src/app/components/quotes-list-item/quotes-list-item.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input } from "@angular/core";
 import { Quote } from "../../types/quotes";
 import { EMPTY_STRING } from "../../constants";
+import appConfig from "../../config/appConfig";
 
 @Component({
 	selector: "app-quotes-list-item",
@@ -11,4 +12,6 @@ export class QuotesListItemComponent {
 	@Input({ required: true }) quote = {} as Quote;
 	@Input({ required: true }) itemIndex = 1;
 	@Input() class = EMPTY_STRING;
+
+	readonly authorDefaultName = appConfig.authorDefaultName;
 }
