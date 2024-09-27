d8801201a17e9f8c5156a5a227fa35e74ea8e03d
Luigi Cavalieri <58509573+LuigiCavalieri@users.noreply.github.com>
2024-09-27 10:16:49 +0200

    Styled pagination menu.

--- fe-angular/src/app/modules/admin.module.ts
+++ fe-angular/src/app/modules/admin.module.ts
@@ -5,6 +5,7 @@ import { QuotesFormComponent } from "../components/quotes-form/quotes-form.compo
 import { ListPageComponent } from "../pages/list-page/list-page.component";
 import { QuotesListComponent } from "../components/quotes-list/quotes-list.component";
 import { QuotesListItemComponent } from "../components/quotes-list-item/quotes-list-item.component";
+import { PaginationMenuComponent } from "../components/pagination-menu/pagination-menu.component";
 
 @NgModule({
 	declarations: [
@@ -13,6 +14,7 @@ import { QuotesListItemComponent } from "../components/quotes-list-item/quotes-l
 		QuotesFormComponent,
 		QuotesListComponent,
 		QuotesListItemComponent,
+		PaginationMenuComponent,
 	],
 	imports: [SharedModule],
 })
