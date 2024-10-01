1e372483047452e1f95c6b704b8c5e99c9d56977
Luigi Cavalieri <58509573+LuigiCavalieri@users.noreply.github.com>
2024-10-01 18:14:25 +0200

    Refactoring.

--- fe-angular/src/app/store/effects/auth.effects.ts
+++ fe-angular/src/app/store/effects/auth.effects.ts
@@ -4,7 +4,7 @@ import * as AuthActions from "../actions/auth.actions";
 import { catchError, exhaustMap, map, tap } from "rxjs/operators";
 import { getStorageItem, removeStorageItem, setStorageItem } from "../../library/local-storage";
 import { LocalStorageKeys } from "../../constants";
-import { EMPTY, of, iif } from "rxjs";
+import { EMPTY, of } from "rxjs";
 import { AuthService } from "../../services/auth.service";
 import { noopAction } from "../actions/app.actions";
 
@@ -23,11 +23,11 @@ export class AuthEffects {
 				ofType(AuthActions.doLogout),
 				tap(() => removeStorageItem(LocalStorageKeys.isLoggedIn)),
 				exhaustMap(({ force }) => {
-					return iif(
-						() => Boolean(force),
-						of(EMPTY),
-						this.authService.logout().pipe(catchError(() => EMPTY))
-					);
+					if (force) {
+						return of(EMPTY);
+					}
+
+					return this.authService.logout().pipe(catchError(() => of(EMPTY)));
 				})
 			),
 		{ dispatch: false }
@@ -36,13 +36,13 @@ export class AuthEffects {
 		this.actions$.pipe(
 			ofType(AuthActions.fetchUser),
 			exhaustMap(() => {
-				return iif(
-					() => Boolean(getStorageItem(LocalStorageKeys.isLoggedIn)),
-					this.authService.me().pipe(
-						map(user => AuthActions.fetchUserSuccess({ user })),
-						catchError(() => of(AuthActions.doLogout({ force: true })))
-					),
-					of(noopAction())
+				if (!getStorageItem(LocalStorageKeys.isLoggedIn)) {
+					return of(noopAction());
+				}
+
+				return this.authService.me().pipe(
+					map(user => AuthActions.fetchUserSuccess({ user })),
+					catchError(() => of(AuthActions.doLogout({ force: true })))
 				);
 			})
 		)
