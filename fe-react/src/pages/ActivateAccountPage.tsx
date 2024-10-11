04b07d92efc2645873485c47a5930252c5a96f9e
Luigi Cavalieri <58509573+LuigiCavalieri@users.noreply.github.com>
2024-10-11 10:13:54 +0200

    Refactoring.

--- fe-react/src/pages/ActivateAccountPage.tsx
+++ fe-react/src/pages/ActivateAccountPage.tsx
@@ -1,4 +1,4 @@
-import { ReactNode, useEffect, useRef, useState } from "react";
+import { ReactNode, useEffect, useMemo, useState } from "react";
 import { Helmet } from "react-helmet-async";
 import { pageItems } from "../config/pageItems";
 import AuthLayout from "../components/AuthLayout/AuthLayout";
@@ -8,68 +8,58 @@ import { isEmail } from "../utils/strings";
 import { ResponseError } from "../types/error";
 import { EMPTY_STRING, ErrorCodes } from "../constants";
 import RouterLink from "../components/RouterLink/RouterLink";
+import { useQuery } from "react-query";
 
 export default function ActivateAccountPage() {
-	// Ensures the mutation is triggered only once when in "strict mode"
-	const mutationTriggeredRef = useRef(false);
 	const [searchParams] = useSearchParams();
 	const [errorMessage, setErrorMessage] = useState<ReactNode>(null);
-	const [isLoading, setIsLoading] = useState<ReactNode>(true);
 
 	const email = searchParams.get("email") || EMPTY_STRING;
 	const activationToken = searchParams.get("activationToken") || EMPTY_STRING;
 	const searchParamsExist = email && activationToken;
+	const searchParamsAreValid = useMemo(
+		() => isEmail(email) && /^[a-z0-9-]+$/i.test(activationToken),
+		[email, activationToken]
+	);
+
+	const { isLoading } = useQuery<unknown, ResponseError>({
+		enabled: searchParamsAreValid && !errorMessage,
+		queryFn: () => activateAccount(email, activationToken),
+		onError: error => {
+			if (error.message === ErrorCodes.accountCannotBeActivated) {
+				setErrorMessage(
+					<>
+						<strong>Your account cannot be activated.</strong>
+						<br />
+						Maybe your activation link has expired.
+						<br />
+						<br />
+						Please, <RouterLink to={pageItems.signup.url}>try to sign up anew</RouterLink>.
+					</>
+				);
+			} else {
+				setErrorMessage(
+					<>
+						<strong>Something didn't work</strong> as expected,
+						<br />
+						please try again later.
+					</>
+				);
+			}
+		},
+	});
 
 	useEffect(() => {
-		if (!searchParamsExist || mutationTriggeredRef.current) {
-			return;
+		if (!searchParamsAreValid) {
+			setErrorMessage(
+				<>
+					<strong>Your account cannot be activated.</strong>
+					<br />
+					Please check your activation link and retry.
+				</>
+			);
 		}
-
-		const triggerActivation = async () => {
-			try {
-				if (isEmail(email) && /^[a-z0-9-]+$/i.test(activationToken)) {
-					await activateAccount(email, activationToken);
-				} else {
-					setErrorMessage(
-						<>
-							<strong>Your account cannot be activated.</strong>
-							<br />
-							Please check your activation link and retry.
-						</>
-					);
-				}
-			} catch (err) {
-				const error = err as ResponseError;
-
-				if (error.message === ErrorCodes.accountCannotBeActivated) {
-					setErrorMessage(
-						<>
-							<strong>Your account cannot be activated.</strong>
-							<br />
-							Maybe your activation link has expired.
-							<br />
-							<br />
-							Please, <RouterLink to={pageItems.signup.url}>try to sign up anew</RouterLink>.
-						</>
-					);
-				} else {
-					setErrorMessage(
-						<>
-							<strong>Something didn't work</strong> as expected,
-							<br />
-							please try again later.
-						</>
-					);
-				}
-			}
-
-			setIsLoading(false);
-		};
-
-		triggerActivation();
-
-		mutationTriggeredRef.current = true;
-	}, [searchParamsExist, email, activationToken]);
+	}, [searchParamsAreValid]);
 
 	if (!searchParamsExist) {
 		return <Navigate to={pageItems.signup.url} />;
